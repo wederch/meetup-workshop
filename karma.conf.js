b0c9f919719f12cdd9d6cd8155a4d204eb47e8b4
Christian Weder <christian.weder@hotmail.com>
2016-08-25 09:13:43 +0200

    Improved linter script with report and added unit test report

--- karma.conf.js
+++ karma.conf.js
@@ -9,13 +9,18 @@ module.exports = function (config) {
       'test/context.js'
     ],
     frameworks: ['mocha', 'chai', 'sinon'],
+    junitReporter: {
+      outputDir: 'build/reports/test',
+      outputFile: 'karma-junit-report.xml',
+      useBrowserName: false
+    },
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
     port: 9876,
     preprocessors: {
       'test/context.js': ['webpack', 'sourcemap']
     },
-    reporters: ['progress'],
+    reporters: ['progress', 'dots', 'junit'],
     singleRun: true,
     webpack: {
       devtool: 'inline-source-map',
